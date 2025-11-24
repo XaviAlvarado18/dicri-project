f6c192aa8a4b0fb0111b1ba7f48eb3827870c913
Javier Alvarado <77702824+XaviAlvarado18@users.noreply.github.com>
2025-11-23 23:37:47 -0600

    fix: docker compose

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,7 +5,7 @@ import sql from "mssql";
 const getDbConfig = () => ({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_HOST || "localhost",
+  server: process.env.DB_SERVER || 'localhost', 
   port: Number(process.env.DB_PORT) || 1433,
   database: process.env.DB_NAME,
   options: {
@@ -39,3 +39,4 @@ export const getConnection = async () => {
 };
 
 export { sql };
+
